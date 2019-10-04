8105903708d610bbf77cf3bc1765fb0aa525e755
PuchkovSasha <Itworkpuchkov@yandex.ru>
2019-10-04 10:14:17 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    
+        var strMain = str.split(''), stack = [], closeIndex, openIndex;
+
+            
+        var sm = bracketsConfig;
+
+        var strBr = String(sm);
+
+        var arr = strBr.split(',');
+
+        var open = [];
+
+        var close=[];
+
+
+    
+
+        for (let i = 0; i < arr.length; i++) {
+            if ((arr[i] === '{')|| (arr[i] === '[') || (arr[i] === '(')     ) {
+                open.push(arr[i]);
+                continue;
+            } 
+            
+            
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+                if ((arr[i] === '}')|| (arr[i] === ']') || (arr[i] === ')')   ){
+                    close.push(arr[i]);
+                    continue;
+                }
+        }
+
+
+
+    for (var i = 0; i <  strMain.length; i++) {
+        openIndex = open.indexOf(strMain[i]);
+        if (openIndex !== -1) {
+
+            stack.push(openIndex);
+            continue;
+        }
+
+        closeIndex = close.indexOf(strMain[i]);
+        if (closeIndex !== -1) {
+
+            openIndex = stack.pop();
+            if (closeIndex !== openIndex) {
+                return false;
+            }
+        }
+    }
+
+
+    if (stack.length !== 0) {
+        return false;
+    } else{
+        return true;
+    }
+
+    
+};
